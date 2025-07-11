d60e6750bb8855917e226254d7b23e39bb5d9bf1
Marcos Vinicius Sousa De Carvalho <mvcarvalho@bbts.com.br>
2025-07-11 15:51:39 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask,jsonify, request, send_file
+from repository.database import db
+from db_models.payment import Payment
+from datetime import datetime, timedelta
+from payments.pix import Pix
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+
+db.init_app(app)
+
+@app.route("/payments/pix", methods=["POST"])
+def create_payment_pix():
+    data = request.get_json()
+    #validacoes
+    if 'value' not in data:
+        return jsonify({"message": "Invalid value"}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], 
+                          expiration_date = expiration_date)
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "the payment has been created" ,
+                    "payment": new_payment.to_dict()})
+
+import os
+from flask import send_file
+
+@app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
+def get_image(file_name):
+    file_path = os.path.join(app.root_path, "static", "img", f"{file_name}.png")
+    
+    return send_file(file_path, mimetype="image/png")
+
+
+@app.route("/payments/pix/confirmation", methods=["POST"])
+def pix_confirmation():
+    return jsonify({"message": "the payment has been confirmed"})
+
+@app.route("/payments/pix/<int:payment_id>", methods=['GET'])
+def payment_pix_page(payment_id):
+    return 'pagamento pix'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
+
+
