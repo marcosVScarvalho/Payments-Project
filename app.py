035f5135d187fe63695678fbe880a8ade662572f
Marcos Vinicius Sousa De Carvalho <mvcarvalho@bbts.com.br>
2025-07-15 15:56:43 -0300

    add payment confirmed

--- app.py
+++ app.py
@@ -49,11 +49,30 @@ def get_image(file_name):
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
+    data = request.get_json()
+    
+    #validations
+    if "bank_payment_id" not in data:
+        return jsonify({"message": "invalid payment data"}),400
+    
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+    if not payment or payment.paid:
+        return jsonify({"message": "payment not found"}),404
+    
+    if data.get("value") != payment.value:
+        return jsonify({"message": "invalid payment data"}),400
+    
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}') 
     return jsonify({"message": "the payment has been confirmed"})
 
 @app.route("/payments/pix/<int:payment_id>", methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if payment.paid:
+        return render_template('confirmed_payment.html')
     
     return render_template('payment.html', payment_id=payment.id, value=payment.value, host="http://127.0.0.1:5000", qr_code=payment.qr_code)
 
